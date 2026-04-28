07b813bedfd8d56a67a3448cbb0fa43463121deb
Rach1981 <102267743+Rach1981@users.noreply.github.com>
2026-04-28 12:09:01 -0500

    Add files via upload

---
+++ project25.ino
@@ -0,0 +1,28 @@
+// Project 25 - Using Inteerrrupts
+#define LED 13 
+void setup() 
+{
+ Serial.begin(9600);
+ pinMode(13, OUTPUT); 
+ attachInterrupt(0, displayMicros, RISING);
+ attachInterrupt(1, displayMillis, RISING);
+ 
+}
+void displayMicros()
+{
+  Serial.write("micros() = ");
+  Serial.println(micros());
+}
+void displayMillis()
+{
+  Serial.write("millis() = ");
+  Serial.println(millis());
+}
+
+void loop() 
+{
+ digitalWrite(LED, HIGH);
+ delay(500);
+ digitalWrite(LED, LOW);
+ delay(500); 
+}
